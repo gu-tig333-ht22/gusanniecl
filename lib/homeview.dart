6ff23a8e0e115e1ea470c4d59ec2d434b7767c41
gusanniecl <gusanniecl@student.gu.se>
2022-09-15 20:12:32 +0200

    Fortsatt utveckla UI + funktionalitet

--- lib/homeview.dart
+++ lib/homeview.dart
@@ -3,50 +3,87 @@
 import 'package:flutter/material.dart';
 import 'package:template/secondview.dart';
 
+// Klass för att skapa task i to do list
+class Todo {
+  String task;
+  bool status;
+
+  Todo(this.task, this.status);
+}
+
+//Hemvy
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var todo = Todo('task', false);
     return Scaffold(
         appBar: AppBar(
           title: Text('To Do List'),
           backgroundColor: Colors.blueGrey,
           elevation: 4,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.more_vert_sharp),
-              onPressed: () {},
-            )
-          ],
+          actions: [_dropdownmenu()],
         ),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-              _checkboxRow(),
-              _checkboxRow(),
-              _checkboxRow(),
-              _checkboxRow(),
-              _checkboxRow(),
-              _checkboxRow()
-            ])),
+        body: _list(),
         floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
+            backgroundColor: Colors.blueGrey,
+            elevation: 6,
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SecondView()));
-            }));
+            },
+            child: Icon(Icons.add)));
   }
 
-Widget _checkboxRow() {
-  return Row(
-    children: [
-      Checkbox(
-        value: false,
+  // Lista som visar todo´s
+  Widget _list() {
+    List<Todo> listOfTodos = [
+      Todo('Task', false),
+      Todo('Task1', false),
+      Todo('Task2', false),
+      Todo('Task3', false),
+      Todo('Task4', false),
+    ];
+    return ListView(
+      children: listOfTodos.map((item) => _item(item)).toList(),
+    );
+  }
+
+// Todo
+  Widget _item(Todo todo) {
+    return CheckboxListTile(
+        value: todo.status,
         onChanged: (val) {},
+        title: Text(todo.task),
+        secondary: Icon(Icons.close),
+        controlAffinity: ListTileControlAffinity.leading,
+        checkColor: Color.fromARGB(255, 5, 29, 70),
+        activeColor: Colors.blueGrey,
+        checkboxShape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)));
+  }
+
+//Filtermeny
+  DropdownMenuItem<String> filter(String value) {
+    return DropdownMenuItem(
+      value: value,
+      child: Text(value),
+    );
+  }
+
+  Widget _dropdownmenu() {
+    return DropdownButtonHideUnderline(
+        child: Container(
+      color: Colors.blueGrey,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            items: [filter('All'), filter('Done'), filter('Undone')],
+            onChanged: (value) {}),
       ),
-      Text('Task'),
-      Icon(Icons.close)
-    ],
-  );
+    ));
+  }
 }
--- lib/secondview.dart
+++ lib/secondview.dart
@@ -1,8 +1,10 @@
 // Vy två, där en uppgift kan adderas till to do listan
 
 import 'package:flutter/material.dart';
+import 'package:template/homeview.dart';
 
 class SecondView extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -10,12 +12,30 @@ class SecondView extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
         elevation: 4,
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Text('What are you going to do?'), Icon(Icons.add)]),
-      ),
+      body: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextField(
+                decoration: InputDecoration(
+                    labelText: 'What are you going to do?',
+                    contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    floatingLabelStyle: TextStyle(color: Colors.grey)),
+                textInputAction: TextInputAction.done,
+                cursorColor: Colors.grey),
+            MaterialButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.blueGrey,
+              textColor: Colors.white,
+              elevation: 4,
+              child: Text('ADD'),
+            )
+          ])),
     );
   }
 }
